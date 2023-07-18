d4a0a73b957c9610c0c1ca49795bd42da08c914c
“Max <mparis@hotmail.cl>
2023-07-18 09:07:43 -0600

    Initial Commit

---
+++ 8ball-mine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8ball-mine
+//
+//  Created by Maximiliano París Gaete on 7/18/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
